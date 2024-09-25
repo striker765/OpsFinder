466662bdd0307b7a02ccbb1ffab889602605f837
striker765 <thiago-carvalho789@outlook.com>
2024-09-25 20:59:52 -0300

    ALLOWED_HOSTS *

--- jobsearch/settings.py
+++ jobsearch/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [] #    Lembrar de adicionar meu dominio
+ALLOWED_HOSTS = ['*'] #    Lembrar de adicionar meu dominio
 
 
 # Application definition
