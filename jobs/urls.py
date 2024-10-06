dec3aec22a13bbaccd8d80cb3963eafe986e21b6
striker765 <thiago-carvalho789@outlook.com>
2024-10-06 01:08:29 -0300

    Mudança path

--- jobs/urls.py
+++ jobs/urls.py
@@ -7,9 +7,9 @@ from django.shortcuts import render
 
 urlpatterns = [
     path('', views.search_jobs, name='search_jobs'),
-    path('show_all_servers/', views.show_all_servers, name='show_all_servers'),
+    path('all_servers/', views.show_all_servers, name='all_servers'),
     path('ajuda/', views.ajuda, name='ajuda'),
-    path('show_all_jobs/', views.show_all_jobs, name='show_all_jobs'),
+    path('all_jobs/', views.show_all_jobs, name='all_jobs'),
 
 ]
 
